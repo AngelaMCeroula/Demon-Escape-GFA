51e40ad3e871ca7bae0a9815fcc809136ef217ab
Angela <115098835+AngelaMCeroula@users.noreply.github.com>
2022-11-02 12:32:45 +0000

    added player controller

---
+++ Assets/Scripts/TopDownPlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownPlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    private Vector2 moveDirection; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // calls for input
+        ProcessInputs();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        //uses Unity's axis input keys "The GetAxis page describes in detail what the axisName for GetAxisRaw means. For example the Horizontal axis is managed by Left and Right, and a and d keys. Other Input Axes can be seen in the Edit->Settings->Input window."
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+   
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+
+    }
+}
