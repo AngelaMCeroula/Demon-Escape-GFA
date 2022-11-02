f7100fd49aa9b61a57582243041c52d26b1ed7b2
Angela <115098835+AngelaMCeroula@users.noreply.github.com>
2022-11-02 12:58:36 +0000

    controller movement change

--- Assets/Scripts/TopDownPlayerController.cs
+++ Assets/Scripts/TopDownPlayerController.cs
@@ -9,12 +9,6 @@ public class TopDownPlayerController : MonoBehaviour
     public Rigidbody2D rb;
     private Vector2 moveDirection; 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
